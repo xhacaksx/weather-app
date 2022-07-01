dbd19fe3ba0a805cc2f745d4bc10e3406ea3e6fd
Akshat <smartakshat007@gmail.com>
2022-07-01 21:51:44 +0530

    added changing background functionality

--- app.js
+++ app.js
@@ -38,6 +38,48 @@ function showWeatherReport(weather) {
 
     let weatherType = document.getElementById('weather');
     weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date=document.getElementById('date');
+    let todaydate =new Date();
+    date.innerText = dateManage(todaydate);
+
+    if(weatherType.textContent=='Clear') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/clear.jpg')"
+    }
+    else if(weatherType.textContent=='Haze') {
+        document.body.style.backgroundImage="url(/home/akshat/weather-app/haze.jpg)"
+    }
+    else if(weatherType.textContent=='Rain') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/rain.jpeg')"
+    }
+    else if(weatherType.textContent=='Clouds') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/clouds.jpeg')"
+    }
+    else if(weatherType.textContent=='Thunderstorm') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/thunderstorm.jpeg')"
+    }
+    else if(weatherType.textContent=='Snow') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/snow.jpeg')"
+    }
+    else if(weatherType.textContent=='Mist') {
+        document.body.style.backgroundImage="url('/home/akshat/weather-app/mist.jpeg')"
+    }
+    
 }
 //date manage
+function dateManage(dateArg) {
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
+}
+
+
 
